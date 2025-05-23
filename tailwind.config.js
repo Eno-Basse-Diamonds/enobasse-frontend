65e2ab3f7a51d99a546e41e03d19c90fb1dbb74f
David Udo <65337929+davidudo@users.noreply.github.com>
2025-05-23 05:50:24 +0100

    chore: add color tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,29 @@ const config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          100: "#502B3A33",
+          200: "#502B3A66",
+          300: "#502B3A99",
+          400: "#502B3ACC",
+          500: "#502B3A",
+        },
+        secondary: {
+          100: "#D1A55933",
+          200: "#D1A55966",
+          300: "#D1A55999",
+          400: "#D1A559CC",
+          500: "#D1A559",
+        },
+        neutral: {
+          100: "#787878",
+          200: "#505050",
+          300: "#282828",
+          400: "#141414",
+          500: "#000000",
+        },
+      },
       fontFamily: {
         primary: ["var(--font-lora)"],
         secondary: ["var(--font-gantari)"],
