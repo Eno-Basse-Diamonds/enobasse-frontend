0588498b17afb591e1bfd4edfb42e567ea685d7f
David Udo <65337929+davidudo@users.noreply.github.com>
2025-06-13 07:54:40 +0100

    refactor(frontend): update home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -117,103 +117,99 @@ export default async function HomePage() {
   ];
 
   return (
-    <>
-      <Header />
-      <main>
-        <HeroSection
-          title="You Deserve The Most Unique Jewelry"
-          description="We create antique jewellery that can be passed down through
+    <main>
+      <HeroSection
+        title="You Deserve The Most Unique Jewelry"
+        description="We create antique jewellery that can be passed down through
             generations - timeless pieces designed to become family heirlooms."
-          image={{
-            src: "/images/hero.webp",
-            alt: "Woman wearing a necklace, ring, and bracelet from Eno Basse",
-          }}
-          buttons={[
-            { text: "See Collections", href: "/collections" },
-            { text: "Shop Engagement", href: "/collections/engagement-rings" },
-          ]}
-        />
+        image={{
+          src: "/images/hero.webp",
+          alt: "Woman wearing a necklace, ring, and bracelet from Eno Basse",
+        }}
+        buttons={[
+          { text: "See Collections", href: "/collections" },
+          { text: "Shop Engagement", href: "/collections/engagement-rings" },
+        ]}
+      />
 
-        <SectionContainer id="categories">
-          <SectionHeading
-            id="categories-heading"
-            title="Explore Eno Basse"
-            description="Our craftsmen work with the finest materials with the sole aim of attaining perfection in every jewellery piece."
-          />
-          <div className="md:hidden">
-            <Carousel itemsPerPage={2} items={carouselItems} />
-          </div>
-          <div className="hidden md:block">
-            <Carousel items={carouselItems} />
-          </div>
+      <SectionContainer id="categories">
+        <SectionHeading
+          id="categories-heading"
+          title="Explore Eno Basse"
+          description="Our craftsmen work with the finest materials with the sole aim of attaining perfection in every jewellery piece."
+        />
+        <div className="md:hidden">
+          <Carousel itemsPerPage={2} items={carouselItems} />
+        </div>
+        <div className="hidden md:block">
+          <Carousel items={carouselItems} />
+        </div>
+      </SectionContainer>
+
+      <SectionContainer id="collections">
+        <SectionHeading
+          id="collections-heading"
+          title="Our Collection"
+          description="When perfectly cut and transformed into a piece of jewellery, diamonds undoubtedly add a touch of uniqueness and glamour to one's style."
+        />
+        <BentoGrid items={bentoItems} />
+      </SectionContainer>
+
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <SectionContainer
+          id="blog"
+          className="bg-[#D1A559] bg-opacity-20 px-4 lg:px-8 py-8 lg:py-16 mt-10 md:mt-20"
+        >
+          <BlogHeader />
+          <BlogContent />
         </SectionContainer>
+      </HydrationBoundary>
 
-        <SectionContainer id="collections">
-          <SectionHeading
-            id="collections-heading"
-            title="Our Collection"
-            description="When perfectly cut and transformed into a piece of jewellery, diamonds undoubtedly add a touch of uniqueness and glamour to one's style."
-          />
-          <BentoGrid items={bentoItems} />
-        </SectionContainer>
+      <HelpSection
+        title="Need Help?"
+        body={[
+          "Selecting the perfect stone and jewellery design is not the easiest task which is why the Eno Basse team is here to assist you every step of the way. We offer consultation services to assist clients in finding the right gemstones to suit their needs.",
+          "Every piece of jewellery tells a story… From glittering necklaces to radiant rings, we lead you to the perfect jewellery to tell your story, the best expression of you. Our team consists of artisans with over 50 years of experience and a wide range of expertise who are eager to help you find and design the perfect piece.",
+        ]}
+        button={{ text: "Contact Us", href: "/contact" }}
+        image={{
+          src: "/images/need-help.webp",
+          alt: "White gold diamond earrings",
+        }}
+      />
 
-        <HydrationBoundary state={dehydrate(queryClient)}>
-          <SectionContainer
-            id="blog"
-            className="bg-[#D1A559] bg-opacity-20 px-4 lg:px-8 py-8 lg:py-16 mt-10 md:mt-20"
-          >
-            <BlogHeader />
-            <BlogContent />
-          </SectionContainer>
-        </HydrationBoundary>
+      <AboutSection
+        title="Company Profile"
+        description={[
+          "Eno Basse was founded with the vision of procuring gemstones to create gorgeous, one-of-a-kind jewellery. Each piece is a true work of art made with the world's finest and most precious gems. We have access to over 1.5 million GIA certified diamonds, which are embodied in our stunning creations.",
+        ]}
+        button={{ text: "About Us", href: "/about" }}
+        image={{
+          src: "/images/founder.webp",
+          alt: "Eno Basse founder",
+        }}
+      />
 
-        <HelpSection
-          title="Need Help?"
-          body={[
-            "Selecting the perfect stone and jewellery design is not the easiest task which is why the Eno Basse team is here to assist you every step of the way. We offer consultation services to assist clients in finding the right gemstones to suit their needs.",
-            "Every piece of jewellery tells a story… From glittering necklaces to radiant rings, we lead you to the perfect jewellery to tell your story, the best expression of you. Our team consists of artisans with over 50 years of experience and a wide range of expertise who are eager to help you find and design the perfect piece.",
-          ]}
-          button={{ text: "Contact Us", href: "/contact" }}
-          image={{
-            src: "/images/need-help.webp",
-            alt: "White gold diamond earrings",
-          }}
-        />
+      <ServicesSection
+        title="Maintenance & Repairs"
+        description={[
+          "A lapidary workshop is a center for cutting, polishing of stones and maintenance of jewellery. Our team offers advice on establishments of these workshops, the requirements and also respond to other enquiries our clients may have.",
+        ]}
+        button={{
+          text: "Contact Us",
+          href: "/maintenance-repairs",
+        }}
+        videoSrc="/videos/maintenance.mp4"
+      />
 
-        <AboutSection
-          title="Company Profile"
-          description={[
-            "Eno Basse was founded with the vision of procuring gemstones to create gorgeous, one-of-a-kind jewellery. Each piece is a true work of art made with the world's finest and most precious gems. We have access to over 1.5 million GIA certified diamonds, which are embodied in our stunning creations.",
-          ]}
-          button={{ text: "About Us", href: "/about" }}
-          image={{
-            src: "/images/founder.webp",
-            alt: "Eno Basse founder",
-          }}
-        />
-
-        <ServicesSection
-          title="Maintenance & Repairs"
-          description={[
-            "A lapidary workshop is a center for cutting, polishing of stones and maintenance of jewellery. Our team offers advice on establishments of these workshops, the requirements and also respond to other enquiries our clients may have.",
-          ]}
-          button={{
-            text: "Contact Us",
-            href: "/maintenance-repairs",
-          }}
-          videoSrc="/videos/maintenance.mp4"
-        />
-
-        <CTASection
-          heading="Want to design your own? Calm, we can do it!"
-          image={{
-            src: "/images/call-to-action.webp",
-            alt: "Ruby ring with gold metal in a box.",
-          }}
-          button={{ text: "Shop Now", href: "/collections" }}
-        />
-      </main>
-      <Footer />
-    </>
+      <CTASection
+        heading="Want to design your own? Calm, we can do it!"
+        image={{
+          src: "/images/call-to-action.webp",
+          alt: "Ruby ring with gold metal in a box.",
+        }}
+        button={{ text: "Shop Now", href: "/collections" }}
+      />
+    </main>
   );
 }
