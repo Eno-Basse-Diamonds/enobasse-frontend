75c93ea35c46c04103fa7908adcb6385a13c91f2
David Udo <65337929+davidudo@users.noreply.github.com>
2025-09-07 13:28:11 +0100

    fix: home page UI

--- src/components/loaders/index.tsx
+++ src/components/loaders/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import {
   AdminBlogSkeletonLoader,
   BlogSectionSkeletonLoader,
@@ -12,6 +13,27 @@ import { AdminCollectionsSkeletonLoader } from "./collections";
 import { AccountLoadingSkeleton } from "./accounts";
 import { AdminReviewsSkeletonLoader } from "./reviews";
 import { DashboardSkeleton } from "./dashboard";
+
+function PageLoader() {
+  return (
+    <div className="flex items-center justify-center min-h-[100dvh] bg-white">
+      <div className="relative flex items-center justify-center">
+        <div className="animate-spin rounded-full h-20 w-20 border-4 border-gray-200 border-t-secondary-500"></div>
+
+        <div className="absolute">
+          <Image
+            src="https://res.cloudinary.com/enobasse/image/upload/v1756506781/logo_gvieez.png"
+            alt="Logo"
+            width={32}
+            height={32}
+            priority
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
 
 export {
   AdminBlogSkeletonLoader,
@@ -29,4 +51,5 @@ export {
   AccountLoadingSkeleton,
   AdminReviewsSkeletonLoader,
   DashboardSkeleton,
+  PageLoader,
 };
