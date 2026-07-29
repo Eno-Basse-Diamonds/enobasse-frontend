c3043dc2a90d2b3d55897de620ac652f84d6d60b
David Udo <udodavid46.ud@gmail.com>
2026-07-29 01:19:35 +0100

    fix: add min-h-screen to sitemap page to push footer down

--- src/app/(company)/sitemap/page.tsx
+++ src/app/(company)/sitemap/page.tsx
@@ -77,7 +77,7 @@ export default async function SitemapPage() {
   ];
 
   return (
-    <div className="px-4 lg:px-8 mt-12 pb-16 text-primary-500">
+    <div className="px-4 lg:px-8 mt-12 pb-16 text-primary-500 min-h-screen">
       <h1 className="font-primary text-3xl md:text-4xl font-semibold text-center mb-12">Sitemap</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {sections.map((section) => (
